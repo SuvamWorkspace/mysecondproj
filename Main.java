aecb202c237973e01017ec91cff9053d6747d986
SuvamWorkspace <152725166+SuvamWorkspace@users.noreply.github.com>
2023-12-03 01:06:57 +0530

    initial commit

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main{
     public static void main(String [] args){
         System.out.println("Hello India how are you");
-        
+        System.out.println("i am good");
+
     }
 }
