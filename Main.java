90af776e05bf9502da6a099feedb153204247bd3
SuvamWorkspace <152725166+SuvamWorkspace@users.noreply.github.com>
2023-12-04 09:02:25 +0530

    added more lines

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ public class Main{
         System.out.println("Hello India how are you");
         System.out.println("i am good");
         System.out.println("where are you from");
+        Student student=new Student();
+        student.sayHello();
 
     }
 }
---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public Student(){
+    System.out.println("Creating Student objects");
+
+}
+public void sayHello(){
+    System.out.println("i am a student yes");
+}
+}
