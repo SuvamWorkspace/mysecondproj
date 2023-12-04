78dd21f64819a2e12eefac511c506752e8275dc8
SuvamWorkspace <152725166+SuvamWorkspace@users.noreply.github.com>
2023-12-04 08:52:54 +0530

    added mistakenly line correctly

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main{
     public static void main(String [] args){
         System.out.println("Hello India how are you");
         System.out.println("i am good");
-        Syste.out.println("where are you from");
+        System.out.println("where are you from");
 
     }
 }
