f312ae4b7a81336dd40aa7e4c0cdfb6cab547e8a
SuvamWorkspace <152725166+SuvamWorkspace@users.noreply.github.com>
2023-12-04 08:49:36 +0530

    added one more line

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main{
     public static void main(String [] args){
         System.out.println("Hello India how are you");
         System.out.println("i am good");
+        Syste.out.println("where are you from");
 
     }
 }
